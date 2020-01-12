807cdccb621a99f809b46f36790054e9170c5465
bogdanlupu <bogdanlupu13@yahoo.ro>
2020-01-12 21:12:23 +0200

    Added cov and corr computation

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import findspark
-from pyspark.sql import SparkSession, Window
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.linalg import Vectors
+from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
 
 import schemas
@@ -26,52 +28,56 @@ def init_spark():
 
 def main():
     spark, sc = init_spark()
-    dummy_switch = '-1.4.0-2018-12-22'
-    # dummy_switch = '_dummy'
+    # dummy_switch = '-1.4.0-2018-12-22'
+    dummy_switch = '_dummy'
 
-    projects_path = f'Libraries.io-open-data-1.4.0/libraries/projects{dummy_switch}.csv'
+    projects_path = f'Libraries.io-open-data-1.4.0/libraries/projects-1.4.0-2018-12-22.csv'
     dependencies_path = f'Libraries.io-open-data-1.4.0/libraries/dependencies{dummy_switch}.csv'
     pwrf_path = f'Libraries.io-open-data-1.4.0/libraries/projects_with_repository_fields{dummy_switch}.csv'
     timestamp_format = 'YYYY-MM-DD HH:mm:ss z'
     # pwrf = spark.read.csv(pwrf_path, header=True, timestampFormat=timestamp_format, schema=schemas.projects_with_repository_fields)
 
-    # projects = spark.read.csv(projects_path, header=True, timestampFormat=timestamp_format,
-    #                           schema=schemas.projects)
+    projects = spark.read.csv(projects_path, header=True, timestampFormat=timestamp_format,
+                              schema=schemas.projects)
     # get_top_ten_projects_for_languages(projects, spark)
-        dependencies = spark.read.csv(dependencies_path, header=True, schema=schemas.dependencies).drop('ID').select('Project ID', 'Dependency Project ID')
-        # dependencies = dependencies.limit(dependencies.count() // 10)
-        dependency_pairs = dependencies.where(F.col('Dependency Project ID').isNotNull())
-        pageranks = dependency_pairs \
-            .select('Project ID') \
-            .union(dependency_pairs
-                   .select('Dependency Project ID')) \
-            .distinct().alias('ID') \
-            .withColumn('pagerank', F.lit(1))
-        dependency_counts = dependency_pairs.groupBy('Project ID').count()
-        # print(pageranks.count(), pageranks.take(5))
-        for it in range(2):
-            print(f'It {it}')
-            dependency_pairs = pageranks \
-                .join(dependency_pairs, 'Project ID') \
-                .join(dependency_counts, 'Project ID') \
-                .withColumn('vote', F.col('pagerank') / F.col('count'))\
-                .drop('pagerank', 'count')
-            vote_sums = dependency_pairs\
-                .groupBy('Dependency Project ID')\
-                .sum('vote')\
-                .withColumnRenamed('sum(vote)', 'vote_sum')
-            # print(vote_sums.take(5))
-            pageranks = pageranks\
-                .join(vote_sums, pageranks['Project ID'] == vote_sums['Dependency Project ID'], how='left')\
-                .drop('pagerank')\
-                .drop('Dependency Project ID')\
-                .withColumn('pagerank', 1 + F.col('vote_sum'))\
-                .drop('vote_sum') \
-                .fillna({'pagerank': 1})
-            pageranks.show(10)
+    dependencies = spark.read.csv(dependencies_path, header=True, schema=schemas.dependencies).drop('ID').select('Project ID', 'Dependency Project ID')
+    dependencies = dependencies.limit(dependencies.count())
+    dependency_pairs = dependencies.where(F.col('Dependency Project ID').isNotNull())
+    pageranks = dependency_pairs \
+        .select('Project ID') \
+        .union(dependency_pairs
+               .select('Dependency Project ID')) \
+        .distinct().alias('ID') \
+        .withColumn('pagerank', F.lit(1))
+    dependency_counts = dependency_pairs.groupBy('Project ID').count()
+    # print(pageranks.count(), pageranks.take(5))
+    for it in range(2):
+        print(f'It {it}')
+        dependency_pairs = pageranks \
+            .join(dependency_pairs, 'Project ID') \
+            .join(dependency_counts, 'Project ID') \
+            .withColumn('vote', F.col('pagerank') / F.col('count')) \
+            .drop('pagerank', 'count')
+        vote_sums = dependency_pairs \
+            .groupBy('Dependency Project ID') \
+            .sum('vote') \
+            .withColumnRenamed('sum(vote)', 'vote_sum')
+        # print(vote_sums.take(5))
+        pageranks = pageranks \
+            .join(vote_sums, pageranks['Project ID'] == vote_sums['Dependency Project ID'], how='left') \
+            .drop('pagerank') \
+            .drop('Dependency Project ID') \
+            .withColumn('pagerank', 1 + F.col('vote_sum')) \
+            .drop('vote_sum') \
+            .fillna({'pagerank': 1})
+        pageranks.show(10)
+    pagerank_and_sourcerank = pageranks.join(projects.select('ID', 'SourceRank'), pageranks['Project ID'] == projects['ID'])\
+        .drop('ID').drop('Project ID')
+    print('Correlation:', pagerank_and_sourcerank.stat.corr('pagerank', 'SourceRank'))
+    print('Covariance:', pagerank_and_sourcerank.stat.cov('pagerank', 'SourceRank'))
+    exit(0)
     with(open('results/pageranks.txt', 'w+')) as f:
-        f.write(pageranks.collect())
-        # exit(0)
+        f.write(pageranks)
         # pageranks = pageranks('pagerank', dependency_pairs.groupBy('Project ID').sum('vote'))
 
     # join_result = projects \
